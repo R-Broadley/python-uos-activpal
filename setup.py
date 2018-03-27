41eb20c8162c744454c6a4b683b3a4bf84da80fa
Rob Broadley <git@rbroadley.co.uk>
2018-03-28 00:10:00 +0100

    Setup initial package

---
+++ setup.py
@@ -0,0 +1,16 @@
+"""Setup for package uos_activpal."""
+
+from distutils.core import setup
+
+setup(
+  name='uos_activpal',
+  packages=['uos_activpal'],  # this must be the same as the name above
+  version='0.1',
+  description='A random test lib',
+  author='Rob Broadley',
+  author_email='software@rbroadley.co.uk',
+  url='https://github.com/R-Broadley/python-uos-activpal',
+  download_url='https://github.com/R-Broadley/python-uos-activpal/archive/0.1.tar.gz',
+  keywords=['activpal', 'accelerometer', 'wearable', 'activity-monitor'],
+  classifiers=[],
+)
